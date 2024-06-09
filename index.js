411d66a0fe858329b6b2eff8557109ff3ccc565f
Taha Khan <162733484+tahakhan4@users.noreply.github.com>
2024-06-09 13:01:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const insert = document.getElementById('id');
+
+window.addEventListener("keydown", (e) => {
+    insert.innerHTML = `
+        <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? "space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    `
+})
